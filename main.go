c0782d130ddf5bedeac04b0b5e35556882e15505
Yuriy <103646851+ReIncepti0N@users.noreply.github.com>
2022-06-05 18:23:55 +0300

    Some changes

--- main.go
+++ main.go
@@ -4,9 +4,10 @@ import "fmt"
 
 func main() {
 
-	massege := sayhello("Юрий!")
+	massege := sayhello("Сергей!")
 	fmt.Println(massege)
+	fmt.Println("Меня зовут Юрий.")
 }
 func sayhello(name string) string {
-	return "Привет" + name
+	return "Привет " + name
 }
